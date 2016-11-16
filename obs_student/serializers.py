0af04efce8caf999502ded8d9fb6f7448da0b24e
dilekuzulmez <dilekuzulmez@gmail.com>
2016-11-16 19:53:52 +0300

    Add department to student api

--- obs_student/serializers.py
+++ obs_student/serializers.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
-        fields = ('url', 'user', 'gender', 'phone','birthdate','image')
+        fields = ('url','department','user', 'gender', 'phone','birthdate','image')
